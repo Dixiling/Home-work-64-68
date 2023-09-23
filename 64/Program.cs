72d80b15be6aa4353a215b71f1b8b215b9f078a8
Конпаку <sputnik-999-mel@mail.ru>
2023-09-23 05:42:28 +0500

    64

---
+++ 64/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Задача 64"); //Напишите программу, которая выведет все натуральные числа в промежутке от N до 1.
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+NaturalToNum(number, count);
+
+void NaturalToNum(int n, int count)
+{
+    if (count > n)
+    {
+        return;
+    }
+    else
+    {
+        NaturalToNum(n, count + 1);
+        Console.Write(count + " ");
+    }
+}
